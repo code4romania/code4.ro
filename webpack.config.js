ea76e6d4c0a39fa5160ceddea08e27577589f9db
Andrei Ioniță <hi@andrei.io>
2019-03-16 20:20:02 +0200

    Fix incomplete build process (#70)
    * Load babel properly
    
    * Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,11 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
 				test: /\.scss$/,
 				use: [
 					{
